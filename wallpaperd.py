c122ab0af1e0d55491ceb188054973ceec0a61cb
Krobix <mrzlime@gmail.com>
2020-12-01 17:21:05 -0500

    add code

---
+++ wallpaperd.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import time
+import os
+import random
+
+PATH = "/sdcard/Pictures/wallpaperd"
+
+while True:
+    chosen = random.choice(os.listdir(PATH))
+    fpath = PATH + chosen
+    os.system(f"termux-wallpaper -f '{fpath}' -l")
+    next_wait_time = random.randint(1200, 7200)
+    time.sleep(next_wait_time)
